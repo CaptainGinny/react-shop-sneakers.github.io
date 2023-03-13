8fa6debc61fdbbd79087874035f3e95d8682a33b
CaptainGinny <ginnymalko@gmail.com>
2023-03-13 12:16:02 +0300

    work css card

--- shop-sneakers/src/App.js
+++ shop-sneakers/src/App.js
@@ -4,9 +4,9 @@ function App() {
     <div className="wrapper">
       <header>
       <div className="headerLeft">
-        <img width={40} height={40} src="/img/logo.png" alt="logo"/>
+        <img width={40} height={40} src="/img/logo.svg" alt="logo"/>
         <div>
-          <h3>React Shop-Sneakers</h3>
+          <h3 className="text-logo">React Shop-Sneakers</h3>
           <p>Магазин лучших кроссовок</p>
         </div>
       </div>
@@ -22,7 +22,25 @@ function App() {
      </header>
      <div className="content">
       <h1>Все кроссовки</h1>
-      ....
+
+
+
+      <div className="card">
+        <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers"/>
+        <p>Мужские Кроссовки<br/>Nike Blazer Mid Suede</p>
+        <div className="cardBottom">
+          <div className="cardPrice">
+            <span>Цена:</span>
+            <b>12 999 руб.</b>
+          </div>
+          <button>
+            <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
+          </button>
+        </div>
+      </div>
+
+
+
      </div>
     </div>
   );
